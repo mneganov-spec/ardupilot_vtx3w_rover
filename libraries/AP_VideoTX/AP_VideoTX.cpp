adbf38a26cf74645a0103862e0bf4cb95341d731
mneganov-spec <mneganov@gmail.com>
2026-06-17 02:22:52 +0300

    VTX power table for AKK TX3000AC (25/250/500/1000/2000/3000mW, up to 35dBm/3W)

--- libraries/AP_VideoTX/AP_VideoTX.cpp
+++ libraries/AP_VideoTX/AP_VideoTX.cpp
@@ -108,19 +108,18 @@ const uint16_t AP_VideoTX::VIDEO_CHANNELS[AP_VideoTX::MAX_BANDS][VTX_MAX_CHANNEL
 // valid power levels from SmartAudio spec, the adjacent levels might be the actual values
 // so these are marked as level + 0x10 and will be switched if a dbm message proves it
 AP_VideoTX::PowerLevel AP_VideoTX::_power_levels[VTX_MAX_POWER_LEVELS] = {
-    // level, mw, dbm, dac
-    { 0xFF,  0,    0, 0    }, // only in SA 2.1
+// level, mw, dbm, dac  -- AKK TX3000AC: 25/250/500/1000/2000/3000 mW
+    { 0xFF,  0,    0,  0    }, // only in SA 2.1
     { 0,    25,   14, 7    },
-    { 0x11, 100,  20, 0xFF }, // only in SA 2.1
-    { 1,    200,  23, 16   },
-    { 0x12, 400,  26, 0xFF }, // only in SA 2.1
+    { 1,    250,  24, 16   },
     { 2,    500,  27, 25   },
-    { 0x12, 600,  28, 0xFF }, // Tramp lies above power levels and always returns 25/100/200/400/600
-    { 3,    800,  29, 40   },
-    { 0x13, 1000, 30, 0xFF }, // only in SA 2.1
-    { 0xFF, 0,    0,  0XFF, PowerActive::Inactive }  // slot reserved for a custom power level
+    { 3,    1000, 30, 40   },
+    { 0x14, 2000, 33, 0xFF },
+    { 0x15, 3000, 35, 0xFF },
+    { 0xFF, 0,    0,  0XFF, PowerActive::Inactive }, // slot reserved for a custom power level
+    { 0xFF, 0,    0,  0XFF, PowerActive::Inactive },
+    { 0xFF, 0,    0,  0XFF, PowerActive::Inactive }
 };
-
 AP_VideoTX::AP_VideoTX()
 {
     if (singleton) {
